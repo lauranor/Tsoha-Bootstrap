00698a2398e6e90ce78195082506e468f2d70103
lauranor <laura.paajanen@helsinki.fi>
2015-09-13 22:46:43 +0300

    add_test_data.sql

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,6 @@
+INSERT INTO Student (name, email) VALUES ('Pirkko', 'mina@email.fi');
+
+INSERT INTO Counsellor (name, password) VALUES ('Tarmo Tietäjä', 'salasana123');
+
+INSERT INTO Subject (subject) VALUES ('Tyhmät kysymykset');
 -- Lisää INSERT INTO lauseet tähän tiedostoon
